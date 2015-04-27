856b6d6e39aede5ecdb8e860ec0ec8518366ba10
legomaniack <ng31415@gmail.com>
2015-04-27 17:41:47 -0500

    init index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+
+<head>
+
+	<title>unquote</title>
+
+	<meta name="viewport" 
+	content="width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes">
+
+	<script src="../components/webcomponentsjs/webcomponents.js">
+	</script>
+
+	<link rel="import" href="../components/font-roboto/roboto.html">
+	<LINK rel="stylesheet" href="style.css" type="text/css">
+	
+</head>
+
+<body unresolved>
+
+</body>
+
+</html>
