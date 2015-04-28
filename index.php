8269e61468b28d314012d3aa5cc25a5b8fe0c153
legomaniack <ng31415@gmail.com>
2015-04-27 21:37:09 -0500

    fixed dependancies?

--- index.php
+++ index.php
@@ -8,10 +8,10 @@
 	<meta name="viewport" 
 	content="width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes">
 
-	<script src="../components/webcomponentsjs/webcomponents.js">
+	<script src="bower_components/webcomponentsjs/webcomponents.js">
 	</script>
 
-	<link rel="import" href="../components/font-roboto/roboto.html">
+	<link rel="import" href="bower_components/font-roboto/roboto.html">
 	<link rel="stylesheet" href="style.css" type="text/css">
 	
 </head>
