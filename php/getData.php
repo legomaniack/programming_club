dbdd004d3425f0013eb684bb4c3becd43bc86c9f
legomaniack <ng31415@gmail.com>
2015-05-06 15:32:52 -0500

    php syntax

--- php/getData.php
+++ php/getData.php
@@ -9,13 +9,15 @@
 	} else {
 		$type = $_GET["type"];
 	}
-	$accepted_values = array("projects","ideas")
-	if (!in_array($type, $accepted_values) $type = "projects";
+	$accepted_values = array("projects","ideas");
+	if (!in_array($type, $accepted_values)) {
+		$type = "projects";
+	}
+	$result = $mysqli->query("SELECT * FROM ".$type) or die($mysqli->error);
 	
-	$result = $mysqli->query("SELECT * FROM ."$type) or die($mysqli->error);
-	
-	while($row=$mysqli->fetch_assoc($result))
+	while($row=$mysqli->fetch_assoc($result)) {
 		$output[]=$row;
+	}
     print(json_encode($output));
     $mysqli->close();
 ?>
