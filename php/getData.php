936ddec984ff2178bd5d51738ae12c0861333e3d
legomaniack <ng31415@gmail.com>
2015-05-06 10:31:00 -0500

    PHP testing

---
+++ php/getData.php
@@ -0,0 +1,21 @@
+<?php 
+	$auth = parse_ini_file('auth.conf');
+	$mysqli = new mysqli("localhost", $auth["username"], $auth["password"], "programming_club");
+	if ($mysqli->connect_errno) {
+		echo "Failed to connect to database";
+	}
+	if 	(!isset($_GET["type"])) {
+		$type = "projects";
+	} else {
+		$type = $_GET["type"];
+	}
+	$accepted_values = array("projects","ideas")
+	if (!in_array($type, $accepted_values) $type = "projects";
+	
+	$result = $mysqli->query("SELECT * FROM ."$type) or die($mysqli->error);
+	
+	while($row=$mysqli->fetch_assoc($result))
+		$output[]=$row;
+    print(json_encode($output));
+    $mysqli->close();
+?>
